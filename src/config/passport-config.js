663a640b0d55ccd4f1be4c9e06c47b770c3bbef8
Kshoes <kx360@nyu.edu>
2019-11-25 22:08:01 -0500

    continue implementing passport.js

--- src/config/passport-config.js
+++ src/config/passport-config.js
@@ -9,44 +9,57 @@ const bcrypt = require('bcrypt');
 
 function initialize(passport, getUserByUsername, getUserById) {
 
-    const authenticateUser = (username, password, done) => {
+    const authenticateUser = async (username, password, done) => {
         const user = getUserByUsername(username);
-
         // User.findOne( {username: username}, async function(err, user) {
 
-        //     if(!user) {
-        //         return done(null, false, {message: 'User not found'});
-        //     }
+            // if(!user) {
+            //     return done(null, false, {message: 'User not found'});
+            // }
     
-        //     try {
-        //         if ( await bcrypt.compare(password, user.password) ) {
-        //             return done(null, user)
-        //         }
-        //         else {
-        //             return done(null, false, {message: 'Password incorrect'})
-        //         }
-        //     }
-        //     catch(err) {
-        //         return done(err);
-        //     }
+            // try {
+            //     if ( await bcrypt.compare(password, user.password) ) {
+            //         return done(null, user)
+            //     }
+            //     else {
+            //         return done(null, false, {message: 'Password incorrect'})
+            //     }
+            // }
+            // catch(err) {
+            //     return done(err);
+            // }
+
 
         // });
 
-        if(!user) {
-            return done(null, false, {message: 'User not found'});
-        }
+            if (err) { return done(err); }
 
-        try {
-            if ( await bcrypt.compare(password, user.password) ) {
-                return done(null, user)
+            if (!user) {
+              return done(null, false, { message: 'User not found' });
             }
-            else {
-                return done(null, false, {message: 'Password incorrect'})
+            
+            if (!user.validPassword(password)) {
+              return done(null, false, { message: 'Incorrect password' });
             }
-        }
-        catch(err) {
-            return done(err);
-        }
+            return done(null, user);
+
+
+
+        // if(user == null) {
+        //     return done(null, false, {message: 'User not found'});
+        // }
+
+        // try {
+        //     if ( await bcrypt.compare(password, user.password) ) {
+        //         return done(null, user)
+        //     }
+        //     else {
+        //         return done(null, false, {message: 'Password incorrect'})
+        //     }
+        // }
+        // catch(err) {
+        //     return done(err);
+        // }
 
     }
 
